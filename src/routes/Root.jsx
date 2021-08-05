8d1e55086e975d4dc216cb5b5b49afbba3d62562
NguyenTaiAnh <anhdv007@gmail.com>
2021-08-05 11:11:06 +0700

    first commit

---
+++ src/routes/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Router } from 'react-router'
+import { Redirect, Route, Switch } from 'react-router-dom'
+function Root() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" component={} />
+            </Switch>
+        </Router>
+    )
+}
+
+export default Root
---
+++ src/state/utils/fetch.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function fetch() {
+  return <div>fetch</div>;
+}
+
+export default fetch;
